174f148934bb6def437b4784e075838619d60307
Daboddavid <Zektay@outlook.com>
2026-04-13 09:46:02 -0400

    ejmeplos de variables

--- prueba.py
+++ prueba.py
@@ -1,1 +1,22 @@
 print("hola mundardooooo god amiwwoooooo")
+
+# creando variables
+Titulo="clima de hoy" #string
+# mas me crece
+diadelmes=13 #int
+mes=4 #int
+
+Temp=22.3 #float
+llueve=False #boolean
+
+
+print(Titulo)
+print("temperatura actual: ", Temp, " grados" )
+print(diadelmes , "-" , mes )
+
+
+
+if llueve:
+    print("tiene que llevar paraguas")
+else:
+    print("puede llevar poleras sin mangas")
