c426336b15795d749e67a39822333cf5748c9c09
daboddavid <zektay@outlook.com>
2026-04-15 15:59:21 -0400

    peo

---
+++ 15-4-2026/file15-4-26  explicacion de for.py
@@ -0,0 +1,22 @@
+# ejemplo y explicacion de for
+
+# for i in range(3):
+#     print("Hola")
+
+# for i in range(5):
+#     print(i)
+
+# pedir un numero al usuario
+
+# num=int(input("digame un numero: "))
+# for i in range(num):
+#     print("hola mariano")
+
+# pedir un numero al usuario y dar tabla de multiplicar hasta 10
+
+
+
+mult=int(input("ingrese un numero para multiplicar: "))
+for i in range (10):
+    print(mult,"X" ,i+1, "=" , mult*(i+1))
+
---
+++ 15-4-2026/file15-4-26.py
@@ -0,0 +1,6 @@
+# print("Ingrese 3 numeros")
+# n1=int(input("numero 1: "))
+# n2=int(input("numero 2: "))
+# n3=int(input("numero 3: "))
+
+
