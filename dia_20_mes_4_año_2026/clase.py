48ce416aad081bcd31f4ff16fe3c5a32411b1adb
Daboddavid <Zektay@outlook.com>
2026-04-20 09:40:21 -0400

    nueva carpeta y concatenaciones

---
+++ dia_20_mes_4_año_2026/clase.py
@@ -0,0 +1,13 @@
+entrenador="Giovanni"
+pkm1=6
+entrenador2="Sabrina"
+pkmn2=7
+
+
+# print(len(entrenador))
+# estos son diferentes maneras para tomar strings
+
+print("El entrenador ", entrenador , "tiene ", pkm1 , " pokemons")
+print("El entrenador ", entrenador2 , "tiene ", pkmn2 , " pokemons")
+print(f"El entrenador {entrenador2} tiene  {pkmn2} pokemons")
+print("El entrenador "+ entrenador2 +" tiene 7 pokemons")
